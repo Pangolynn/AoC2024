5aef6191b6c125bd1150756c0cf787043791740a
Sammi <sami.berlynn@proton.me>
2024-12-02 13:16:49 +0100

    Refactor part 1 into functions

--- Program.cs
+++ Program.cs
@@ -15,6 +15,20 @@ class Program
             return;
         }
 
+        (List<int> col1, List<int> col2) = ReadColumnsFromFile(filePath);
+        
+        if (col1.Count != col2.Count)
+        {
+            Console.WriteLine("The number of items in the list are different.");
+            return;
+        }
+        
+        Console.WriteLine($"Total distance: {CalculateDistance(col1, col2)}");
+ 
+    }
+
+    static (List<int>, List<int>) ReadColumnsFromFile(string filePath)
+    {
         List<int> col1 = new List<int>();
         List<int> col2 = new List<int>();
 
@@ -22,7 +36,7 @@ class Program
         {
             string[] parts = line.Split("   ");
 
-            if (parts.Length >= 2 && 
+            if (parts.Length >= 2 &&
                 int.TryParse(parts[0], out int num1) &&
                 int.TryParse(parts[1], out int num2))
             {
@@ -33,26 +47,25 @@ class Program
             {
                 Console.WriteLine($"Skipping invalid line: {line}");
             }
-            
         }
+        return (col1, col2);
+    }
 
+    static int CalculateDistance(List<int> col1, List<int> col2)
+    {
+        // Match the left and right locations in ascending order
         col1.Sort();
         col2.Sort();
 
         int distance = 0;
-        
-        if (col1.Count != col2.Count)
-        {
-            Console.WriteLine("The number of items in the list are different.");
-            return;
-        }
 
+        // find the distance between each left/right pair and add 
+        // each distance to find the total distance between the lists
         for (int i = 0; i < col1.Count; i++)
         {
-            distance += ((Math.Abs(col1[i] - col2[i])));
+            distance += (Math.Abs(col1[i] - col2[i]));
         }
-
-        Console.WriteLine($"Total distance: {distance}");
- 
+        
+        return distance;
     }
 }
--- obj/Debug/net8.0/AdventOfCode2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/AdventOfCode2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75b338a0015230889a3e8728cfe243df0e61a2fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
